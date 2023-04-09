0481c7fa2c1ae52c622db151d11fef0c4d7eb1e2
Simon Heimlicher <sh@srvr.ch>
2023-04-09 18:56:42 +0200

    Improve layout of excerpt

--- assets/scripts/claris/legacy.js
+++ assets/scripts/claris/legacy.js
@@ -1,16 +1,7 @@
 // console.log('BEGIN legacy.js');
+// import 'object-fit-images'; // Not sufficient. Need to call objectFitImages()
 import objectFitImages from 'object-fit-images';
 function initializeObjectFitImages() {
-  // FIXME: Not required
-  // // CSS rules
-  // const objectFitImagesRules = ".object-fit-cover { font-family: 'object-fit: cover; object-position: center center;' }";
-
-  // const css = document.createElement('style');
-  // css.type = 'text/css';
-  // if (css.styleSheet) css.styleSheet.cssText = objectFitImagesRules; // Support for IE
-  // else css.appendChild(document.createTextNode(objectFitImagesRules)); // Support for the rest
-  // document.getElementsByTagName("head")[0].appendChild(css);
-
   objectFitImages();
 }
 window.addEventListener("DOMContentLoaded", initializeObjectFitImages);
@@ -18,14 +9,17 @@ window.addEventListener("DOMContentLoaded", initializeObjectFitImages);
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-// polyfills
-/*
-import 'lazysizes/plugins/respimg/ls.respimg';
+// LazySizes polyfills
+// FIXME: no effect in IE 11 visible
+// import 'lazysizes/plugins/respimg/ls.respimg';
 
-if (!('object-fit' in document.createElement('a').style)) {
-  require('lazysizes/plugins/object-fit/ls.object-fit');
-}
-*/
+// FIXME: no effect in IE 11 visible
+// if (!('object-fit' in document.createElement('a').style)) {
+//   require('lazysizes/plugins/object-fit/ls.object-fit');
+// }
+
+
+// import 'picturefill';
 
 import 'scripts/claris/theme/minimal';
 // console.log('END   legacy.js');
