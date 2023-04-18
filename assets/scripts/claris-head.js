f00dcc207388ab283a892700c38cee722ac2c3cd
Simon Heimlicher <sh@srvr.ch>
2023-04-18 02:22:03 +0200

    Refactored images and schema.org metadata

--- assets/scripts/claris-head.js
+++ assets/scripts/claris-head.js
@@ -5,5 +5,12 @@ import 'scripts/claris/theme/init';
 import 'scripts/claris/theme/color-mode';
 
 import 'lazysizes';
+
+// Since we use CSS attribute 'object-fit', lazy loading cannot infer
+// the size of the displayed image from the <img> element.
+// The plugin 'parant-fit' could take care of this gap but probably requires
+// some changes to the partial 'claris/responsive-image' to work properly.
+// https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/parent-fit
+// https://github.com/aFarkas/lazysizes/issues/508#issuecomment-397545775
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 // console.log('END   claris-head');
