4bc6f6892285771199abc2f8231f264518f3b828
Simon Heimlicher <sh@srvr.ch>
2023-05-02 03:47:40 +0200

    Move lazysizes to body

--- assets/scripts/claris-body.js
+++ assets/scripts/claris-body.js
@@ -1,4 +1,15 @@
 // console.log('BEGIN claris-body');
 import 'scripts/claris/theme/minimal';
 import 'scripts/claris/theme/enhanced';
+
+import 'lazysizes';
+
+// Since we use CSS attribute 'object-fit', lazy loading cannot infer
+// the size of the displayed image from the <img> element.
+// The plugin 'parant-fit' could take care of this gap but probably requires
+// some changes to the partial 'claris/responsive-image' to work properly.
+// https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/parent-fit
+// https://github.com/aFarkas/lazysizes/issues/508#issuecomment-397545775
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
 // console.log('END   claris-body');
