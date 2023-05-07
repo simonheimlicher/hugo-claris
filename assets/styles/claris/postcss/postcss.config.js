b07f8e66a9c6de052fe2c5ebbbb8ccb98265ae2a
Simon Heimlicher <sh@srvr.ch>
2023-05-07 23:57:47 +0200

    Clean up code for minification of CSS and JS

--- assets/styles/claris/postcss/postcss.config.js
+++ assets/styles/claris/postcss/postcss.config.js
@@ -1,15 +1,23 @@
+// Source: https://purgecss.com/guides/hugo.html
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+  },
+  // https://github.com/gohugoio/hugo/issues/10338
+  // https://discourse.gohugo.io/t/purgecss-and-highlighting/41021
+  safelist: {
+    greedy: [/highlight/, /chroma/, /open$/]
+  },
+  // variables: true,
+});
+
 module.exports = {
   plugins: [
-    //  require("autoprefixer")({}),
-    // Source: https://purgecss.com/guides/hugo.html
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./hugo_stats.json"],
-      defaultExtractor: (content) => {
-        const els = JSON.parse(content).htmlElements;
-        return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-      },
-      safelist: [],
-      // variables: true,
-    }),
+    require("autoprefixer")({}),
+    // FIXME: Need to pass in the environments that should enable PurgeCSS
+    // as a parameter instead of hardcoding
+    ...(['stage', 'prod', 'production'].includes(process.env.HUGO_ENVIRONMENT) ? [purgecss] : []),
   ]
 };
