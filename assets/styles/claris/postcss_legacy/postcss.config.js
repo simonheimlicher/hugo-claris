ae130aa4e127401f5d340eb462deb14eeeb91e9d
Simon Heimlicher <sh@srvr.ch>
2023-05-08 14:42:07 +0200

    Fix navigation menu in legacy browsers

--- assets/styles/claris/postcss_legacy/postcss.config.js
+++ assets/styles/claris/postcss_legacy/postcss.config.js
@@ -1,3 +1,18 @@
+// Source: https://purgecss.com/guides/hugo.html
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+  },
+  // https://github.com/gohugoio/hugo/issues/10338
+  // https://discourse.gohugo.io/t/purgecss-and-highlighting/41021
+  safelist: {
+    greedy: [/highlight/, /chroma/, /open$/]
+  },
+  // variables: true,
+});
+
 module.exports = {
   plugins: [
      require("autoprefixer")({
@@ -16,14 +31,8 @@ module.exports = {
      require('postcss-custom-properties')({
       preserve: true,
      }),
-    // Source: https://purgecss.com/guides/hugo.html
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./hugo_stats.json"],
-      defaultExtractor: (content) => {
-        const els = JSON.parse(content).htmlElements;
-        return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-      },
-      safelist: [],
-    }),
+    // FIXME: Need to pass in the environments that should enable PurgeCSS
+    // as a parameter instead of hardcoding
+    ...(['stage', 'prod', 'production'].includes(process.env.HUGO_ENVIRONMENT) ? [purgecss] : []),
   ]
 };
