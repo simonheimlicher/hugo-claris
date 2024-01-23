7c69f657dba1a5ffe1f4a97bf88521b0574f7bc6
Simon Heimlicher <simon.github@heimlicher.com>
2024-01-23 18:41:29 +0100

    feat: make obfuscated-link and -email more robust
    - Improve handling of surrounding whitespace
    - Ensure that multiple obfuscated links with the same DOM node as parent are decrypted properly

--- assets/scripts/claris/theme/obfuscated-link.js
+++ assets/scripts/claris/theme/obfuscated-link.js
@@ -1,6 +1,8 @@
 export function obfuscatedLinkInit() {
   const obfuscatedLinkKeyOneTimePasswordAttr = 'data-obfuscated-link-one-time-password';
   const nodeList = document.querySelectorAll('[' + obfuscatedLinkKeyOneTimePasswordAttr + ']');
+  const nodesArray = Array.from(nodeList); // Convert NodeList to an array
+
   // console.log("obfuscated-link: deobufscating links: ", nodeList);
 
   const indexToChar = [
@@ -139,8 +141,15 @@ export function obfuscatedLinkInit() {
     "=": 64,
   };
 
-  for (var idx = 0; idx < nodeList.length; ++idx) {
-    const node = nodeList[idx];
+  // If the nodeList contains multiple elements with the same parentNode, the `.parentNode`
+  // property will become undefined when we replace the first child.
+  let postponedNodes = []
+  for (const node of nodesArray) {
+    if (!node.parentNode) {
+      postponedNodes.push(node);
+      continue; // Skip this iteration if parentNode is null
+    }
+
     const parentNode = node.parentNode;
     const scheme = node.dataset['obfuscatedLinkScheme'];
     const text = node.dataset['obfuscatedLinkText'];
@@ -163,9 +172,26 @@ export function obfuscatedLinkInit() {
     const url = new URL(decrypted);
     // console.log('encrypted: ', encrypted, ' oneTimePassword: ', oneTimePassword, ' decryptedEncoded: ', decryptedEncoded, ' address: ', address);
     let unobfuscatedNode = document.createElement(node.tagName)
+    const targetAttribute = (scheme.startsWith("http")) ? ' target="_blank" ' : '';
     unobfuscatedNode.innerHTML = '<' + 'a hr' + 'ef="' + scheme + address
-      + '" title="' + title.replace('OBFUSCATED', address) + '">' + (text ? text.replace('OBFUSCATED', address) : address) + '</a>';
+      + '" title="' + title.replace('OBFUSCATED', address) + '"' + targetAttribute + '>'
+      + (text ? text.replace('OBFUSCATED', address) : address) + '</a>';
     parentNode.replaceChild(unobfuscatedNode, node)
-    parentNode.innerHTML = parentNode.innerHTML.replace(/^(.*[^ ])\n/, '$1').replace(/\n+(.)$/, '$1')
+    // Eliminate trailing newline if and only if it is followed by a non-letter character
+    // based on the unicode category not begin "letter": `\P{L}`
+    // Note: for this regex to work, we need to switch to unicode mode by appending `u`
+    // parentNode.innerHTML = parentNode.innerHTML.replace(/^(.*[^ ])\n/, '$1').replace(/\n+(.)$/, '$1')
+    // parentNode.innerHTML = parentNode.innerHTML.replace(/^(.*)\n/u, '$1');
+    const searchPattern = String.raw`^(.*)\n(\P{L}+)`;
+    const flags = 'us';
+    const replacement = String.raw`$1$2`;
+    const stripNewlineRE = new RegExp(searchPattern, flags);
+    const replacementLog = `UUU$1VVV$2WWW`;
+    console.log(parentNode.innerHTML.replace(stripNewlineRE, replacementLog));
+    parentNode.innerHTML = parentNode.innerHTML.replace(stripNewlineRE, replacement);
+  }
+  if (postponedNodes.length > 0) {
+    // Re-run the function at the next animation frame
+    requestAnimationFrame(obfuscatedLinkInit);
   }
 }
