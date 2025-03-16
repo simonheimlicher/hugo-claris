84eccc61aada9043382a28549fac436db1faae85
Simon Heimlicher <simon.github@heimlicher.com>
2025-03-16 13:44:42 +0100

    refactor: simplify methods

--- assets/scripts/claris/enhanced/lazy-loading.js
+++ assets/scripts/claris/enhanced/lazy-loading.js
@@ -35,103 +35,118 @@ let lazyLoadingInitDone = false;
 // in a function so we can re-run if data is added dynamically
 export function lazyLoadingInit() {
   const PREFIX = false; // 'lazy-loading:';
-  // check if loading attribute supported
+  // Use native lazy loading if loading attribute supported
   if ('loading' in HTMLImageElement.prototype) {
-    // NOTE: Safari 16.4 does not respond to swapping in new value for `src`, therefore we schedule a
-    // second execution after the load event
-    const isSafariBrowser = navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1;
+    initializeNativeLazyLoading(PREFIX);
 
-    // get all <img> and <source> elements with `data-src` and `data-srcset` attributes
-    const images = document.querySelectorAll('img[data-src]');
-    const sources = document.querySelectorAll('source[data-srcset]');
-
-    if (!lazyLoadingInitDone) {
-      deb(PREFIX, 'Browser supports native lazy loading. Swap in value of attribute `data-src` for `src` and `data-srcset` for `srcset`');
-    }
-    else {
-      deb(PREFIX, 'Second round');
-    }
-
-    // loop through <img>s setting the src attribute and srcset and sizes if present
-    // but only if there is no data-media attribute
-    for (let img of images) {
-      if (!img.dataset.media?.includes('prefers-color-scheme')) {
-        deb(PREFIX, 'Swapping in src for ', img);
-        try {
-          img.src = img.dataset.src;
-          if (!isSafariBrowser || lazyLoadingInitDone) {
-            delete img.dataset.src;
-          }
-          const srcset = img.dataset.srcset;
-          if (srcset) {
-            deb(PREFIX, 'Swapping in srcset for ', img);
-            img.srcset = srcset;
-            if (!isSafariBrowser || lazyLoadingInitDone) {
-              delete img.dataset.srcset;
-            }
-          }
-          const sizes = img.dataset.sizes;
-          if (sizes) {
-            img.sizes = sizes;
-            if (!isSafariBrowser || lazyLoadingInitDone) {
-              delete img.dataset.sizes;
-            }
-          }
-        }
-        catch (e) {
-          deb(PREFIX, 'Failed to swap in src or srcset for img=', img, '\nException: ', e);
-        }
-      }
-    }
-
-    // loop through <source>s setting the srcset attribute and sizes if present
-    // but only if there is no data-media attribute
-    for (let source of sources) {
-      if ( !source.dataset.media?.includes('prefers-color-scheme')) {
-        try {
-          source.srcset = source.dataset.srcset;
-          if (!isSafariBrowser || lazyLoadingInitDone) {
-            delete source.dataset.srcset;
-          }
-          const sizes = source.dataset.sizes;
-          if (sizes) {
-            source.sizes = sizes;
-            if (!isSafariBrowser || lazyLoadingInitDone) {
-              delete source.dataset.sizes;
-            }
-          }
-        }
-        catch (e) {
-          deb(PREFIX, 'Failed to swap in srcset for source=', source, '\nException: ', e);
-        }
-      }
-    }
-
-    if (isSafariBrowser && ! lazyLoadingInitDone) {
-      deb(PREFIX, 'Schedule a second round of swapping in values for `src` and `srcset`');
-      window.addEventListener('load', lazyLoadingInit);
-      if (document.readyState === "loaded") {
-        // If the `load` event has already fired, let's just execute again right now
-        deb(PREFIX, 'document.readyState='
-          + document.readyState + '. Call init initLazyLoading again right now');
-        lazyLoadingInit();
-      }
-    }
-
-  // if loading attribute is not supported
-  // check we haven't already loaded the library
+  // if loading attribute is not supported, use `LazySizes` unless it has already been imported
   } else if (!lazyLoadingInitDone) {
-    // create script element with src pointing to our library and add to document
-    const lazySizesScript = document.createElement('script');
-    lazySizesScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
-    document.body.appendChild(lazySizesScript);
-
-    const lazySizesParentFitScript = document.createElement('script');
-    lazySizesParentFitScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/parent-fit/ls.parent-fit.min.js';
-    document.body.appendChild(lazySizesParentFitScript);
-
-    deb(PREFIX, 'Browser does not support native lazy loading. Imported lazySizes=', lazySizesScript);
+    // create script element with src pointing to `LazySizes` and add to document
+    initializeLazySizes(PREFIX);
 
   }
   lazyLoadingInitDone = true;
+}
+
+function initializeNativeLazyLoading(PREFIX) {
+  const isSafariBrowser = navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1;
+
+  // get all <img> and <source> elements with `data-src` and `data-srcset` attributes
+  const images = document.querySelectorAll('img[data-src]');
+  const sources = document.querySelectorAll('source[data-srcset]');
+
+  if (!lazyLoadingInitDone) {
+    deb(PREFIX, 'Browser supports native lazy loading. Swap in value of attribute `data-src` for `src` and `data-srcset` for `srcset`');
+  }
+  else {
+    deb(PREFIX, 'Second round');
+  }
+
+  // loop through <img>s setting the src attribute and srcset and sizes if present
+  // but only if there is no data-media attribute
+  processImages(PREFIX, images, isSafariBrowser);
+
+  // loop through <source>s setting the srcset attribute and sizes if present
+  // but only if there is no data-media attribute
+  processSources(PREFIX, sources, isSafariBrowser);
+
+  // NOTE: Safari 16.4 does not respond to swapping in new value for `src`, therefore we schedule a
+  // second execution after the load event
+  if (isSafariBrowser && !lazyLoadingInitDone) {
+    deb(PREFIX, 'Schedule a second round of swapping in values for `src` and `srcset`');
+    window.addEventListener('load', lazyLoadingInit);
+    if (document.readyState === "loaded") {
+      // If the `load` event has already fired, let's just execute again right now
+      deb(PREFIX, 'document.readyState='
+        + document.readyState + '. Call init initLazyLoading again right now');
+      lazyLoadingInit();
+    }
+  }
+}
+
+function processSources(PREFIX, sources, isSafariBrowser) {
+  for (let source of sources) {
+    if (!source.dataset.media?.includes('prefers-color-scheme')) {
+      try {
+        source.srcset = source.dataset.srcset;
+        if (!isSafariBrowser || lazyLoadingInitDone) {
+          delete source.dataset.srcset;
+        }
+        const sizes = source.dataset.sizes;
+        if (sizes) {
+          source.sizes = sizes;
+          if (!isSafariBrowser || lazyLoadingInitDone) {
+            delete source.dataset.sizes;
+          }
+        }
+      }
+      catch (e) {
+        deb(PREFIX, 'Failed to swap in srcset for source=', source, '\nException: ', e);
+      }
+    }
+  }
+}
+
+function processImages(PREFIX, images, isSafariBrowser) {
+  for (let img of images) {
+    if (!img.dataset.media?.includes('prefers-color-scheme')) {
+      deb(PREFIX, 'Swapping in src for ', img);
+      try {
+        img.src = img.dataset.src;
+        if (!isSafariBrowser || lazyLoadingInitDone) {
+          delete img.dataset.src;
+        }
+        const srcset = img.dataset.srcset;
+        if (srcset) {
+          deb(PREFIX, 'Swapping in srcset for ', img);
+          img.srcset = srcset;
+          if (!isSafariBrowser || lazyLoadingInitDone) {
+            delete img.dataset.srcset;
+          }
+        }
+        const sizes = img.dataset.sizes;
+        if (sizes) {
+          img.sizes = sizes;
+          if (!isSafariBrowser || lazyLoadingInitDone) {
+            delete img.dataset.sizes;
+          }
+        }
+      }
+      catch (e) {
+        deb(PREFIX, 'Failed to swap in src or srcset for img=', img, '\nException: ', e);
+      }
+    }
+  }
+}
+
+function initializeLazySizes(PREFIX) {
+  const lazySizesScript = document.createElement('script');
+  lazySizesScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+  document.body.appendChild(lazySizesScript);
+
+  const lazySizesParentFitScript = document.createElement('script');
+  lazySizesParentFitScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/parent-fit/ls.parent-fit.min.js';
+  document.body.appendChild(lazySizesParentFitScript);
+
+  deb(PREFIX, 'Browser does not support native lazy loading. Imported lazySizes=', lazySizesScript);
 }
