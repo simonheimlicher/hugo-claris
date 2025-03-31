3faab1443ce99928313d38d2c1f4d8531b53346d
Simon Heimlicher <simon.github@heimlicher.com>
2025-04-01 00:17:11 +0200

    feature: generate multiple bundles per target

--- assets/scripts/claris-head_async.js
+++
@@ -1,7 +0,0 @@
-/* File='scripts/claris-head_async.js': hugo.Environment='{{ hugo.Environment }}' .Page='{{ .Page }}' .MediaType='{{ .MediaType }}' */
-
-// NOTE: JavaScript code in this file is executed as a module in the <head>
-// This means that
-// * Loading is "defer" on browsers that support the "module" attribute
-//   while all other browsers ignore this script
-// * Execution is asynchronous, i.e., the script has the "async" attribute
---
+++ assets/scripts/claris/target/body/04_body_custom.js
@@ -0,0 +1,9 @@
+/* File='scripts/claris/target/body/04_body_custom.js': hugo.Environment='{{ hugo.Environment }}' .Page='{{ .Page }}' .MediaType='{{ .MediaType }}' */
+
+// NOTE: JavaScript code in this file is executed as a module at the bottom of <body>
+// This means that
+// * Loading is "defer" on browsers that support the "module" attribute
+//   while all other browsers ignore this script
+// * Execution is synchronous, i.e., the script does not have the "async" attribute
+
+// Add your custom JavaScript code to this file
